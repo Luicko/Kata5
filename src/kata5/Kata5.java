6adde30dd516247ca4d9401c534ed858f64ef291
Luicko <Luicko@Ninita>
2020-01-23 18:31:53 +0000

    Version 1

---
+++ src/kata5/Kata5.java
@@ -0,0 +1,39 @@
+package kata5;
+
+import java.util.List;
+import kata5.model.Histogram;
+import kata5.model.Mail;
+import kata5.view.HistogramDisplay;
+import kata5.view.MailHistogramBuilder;
+import kata5.view.MailListReaderBD;
+
+public class Kata5 {
+
+    private List<Mail> mailList;
+    private Histogram<String> histogram;
+    private HistogramDisplay histoDisplay;
+
+    public static void main(String[] args) {
+        Kata5 kata5 = new Kata5();
+        kata5.execute();
+    }
+
+    public void execute() {
+        input();
+        process();
+        output();
+    }
+
+    public void input() {
+        mailList = MailListReaderBD.read();
+    }
+
+    public void process() {
+        histogram = MailHistogramBuilder.build(mailList);
+    }
+
+    public void output() {
+        histoDisplay = new HistogramDisplay(histogram);
+        histoDisplay.execute();
+    }
+}
